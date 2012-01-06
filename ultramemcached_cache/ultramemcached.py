e6389e770eb21c463a26d947514c8cc5255390dc
nicholasserra <nicks@ydekproductions.com>
2012-01-06 12:22:34 -0500

    added compression

--- ultramemcached_cache/ultramemcached.py
+++ ultramemcached_cache/ultramemcached.py
@@ -16,6 +16,15 @@ try:
 except ImportError:
     from StringIO import StringIO
 
+try:
+    from zlib import compress, decompress
+    _supports_compress = True
+except ImportError:
+    _supports_compress = False
+    # quickly define a decompress just in case we recv compressed data.
+    def decompress(val):
+        raise _Error("received compressed data but I don't support compression (import error)")
+
 _FLAG_PICKLE  = 1<<0
 _FLAG_INTEGER = 1<<1
 _FLAG_LONG    = 1<<2
@@ -80,7 +89,7 @@ class BaseUltraMemcachedCache(BaseCache):
             timeout += int(time.time())
         return timeout
 
-    def _val_to_store(self, val):
+    def _val_to_store(self, val, min_compress_len=0):
         flags = 0
         if isinstance(val, str):
             pass
@@ -103,10 +112,27 @@ class BaseUltraMemcachedCache(BaseCache):
                 pickler = self.pickler(file, 0)
             pickler.dump(val)
             val = file.getvalue()
+            
+        lv = len(val)
+        # We should try to compress if min_compress_len > 0 and we could
+        # import zlib and this string is longer than our min threshold.
+        
+        if min_compress_len and _supports_compress and lv > min_compress_len:
+            print 'in compress'
+            comp_val = compress(val)
+            # Only retain the result if the compression result is smaller
+            # than the original.
+            if len(comp_val) < lv:
+                print 'set compress flags'
+                flags |= _FLAG_COMPRESSED
+                val = comp_val
+
         return val, flags
     
     def _clean_val(self, buf, flags, default=None):
-        if  flags == 0:
+        if flags & _FLAG_COMPRESSED:
+            buf = decompress(buf)
+        if  flags == 0 or flags == _FLAG_COMPRESSED:
             # Either a bare string or a compressed string now decompressed...
             val = buf
         elif flags & _FLAG_INTEGER:
@@ -125,9 +151,9 @@ class BaseUltraMemcachedCache(BaseCache):
         return val
         
     @_make_conn
-    def add(self, client, key, val, timeout=0, version=None):
+    def add(self, client, key, val, timeout=0, version=None, min_compress_len=0):
         key = self.make_key(key, version=version)
-        val, flags = self._val_to_store(val)
+        val, flags = self._val_to_store(val, min_compress_len)
         stored = client.add(key, val, self._get_memcache_timeout(timeout), flags)
         return stored == "STORED"
 
@@ -143,9 +169,9 @@ class BaseUltraMemcachedCache(BaseCache):
         return value
 
     @_make_conn
-    def set(self, client, key, val, timeout=0, version=None):
+    def set(self, client, key, val, timeout=0, version=None, min_compress_len=0):
         key = self.make_key(key, version=version)
-        val, flags = self._val_to_store(val)
+        val, flags = self._val_to_store(val, min_compress_len)
         stored = client.set(key, val, self._get_memcache_timeout(timeout), flags)
         return stored == "STORED"
         
@@ -206,7 +232,7 @@ class BaseUltraMemcachedCache(BaseCache):
         return val
 
     @_make_conn
-    def set_many(self, client, data, timeout=0, version=None):
+    def set_many(self, client, data, timeout=0, version=None, min_compress_len=0):
         #not any faster because there is no set_many in umemcached
 
         if not isinstance(data, dict):
@@ -219,30 +245,9 @@ class BaseUltraMemcachedCache(BaseCache):
             safe_data[key] = value
 
         for key, val in safe_data.items():
-            flags = 0
-            if isinstance(val, str):
-                pass
-            elif isinstance(val, int):
-                flags |= _FLAG_INTEGER
-                val = "%d" % val
-                # force no attempt to compress this silly string.
-                min_compress_len = 0
-            elif isinstance(val, long):
-                flags |= _FLAG_LONG
-                val = "%d" % val
-                # force no attempt to compress this silly string.
-                min_compress_len = 0
-            else:
-                flags |= _FLAG_PICKLE
-                file = StringIO()
-                if self.picklerIsKeyword:
-                    pickler = self.pickler(file, protocol = self.pickleProtocol)
-                else:
-                    pickler = self.pickler(file, 0)
-                pickler.dump(val)
-                val = file.getvalue()
+            final_value, flags = self._val_to_store(val, min_compress_len)
 
-            client.set(key, val, self._get_memcache_timeout(timeout), flags)
+            client.set(key, final_value, self._get_memcache_timeout(timeout), flags)
 
     @_make_conn
     def delete_many(self, client, keys, version=None):
